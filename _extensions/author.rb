ed4a57cf06e1f707386495c368d8545b91c7fb7a
Alex Young <alex@helicoid.net>
2009-11-03 19:05:17 +0000

    Added some ads and made the site wider

--- _extensions/author.rb
+++ _extensions/author.rb
@@ -2,9 +2,12 @@ module GitAuthorHelper
   # Example in a template:
   #   By {{ page.url | author }}
   def author(url)
-    file_name = File.join '_posts', "*#{url.split('/').last}*"
-    post_file = Dir[file_name].first
-    `git log --pretty=format:"%an" #{post_file}`
+    if url
+      file_name = File.join '..', '_posts', "*#{url.split('/').last}*"
+      post_file = Dir[file_name].first
+      names = `git log --pretty=format:\"%an\" #{post_file}`
+      names.split("\n").first
+    end
   rescue
     puts "Error inserting author: #{$!}"
   end
